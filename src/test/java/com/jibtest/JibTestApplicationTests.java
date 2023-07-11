4ecb168b2793bdef75f5a77116b9d3342a7b6e81
fosong98 <fosong98@gmail.com>
2023-07-11 12:04:27 +0900

    test

---
+++ src/test/java/com/jibtest/JibTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jibtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
